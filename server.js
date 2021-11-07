5755e19c82f4f6ce7919b456ee9e2e42c45328cd
M H Y <bluestar2022@gmail.com>
2021-11-07 22:40:22 +0300

    general port

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(cors())
 
 app.get('/', (req,res) => {
     res.send('this is working')
-    // res.send(database.users)
+    // res.send(database.users) trying manual local database
 })
 
 app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt) })
